7f5c2992f8d6a12548536d2335474e071a95fa2c
Ambikesh <kumarambikesh509@gmail.com>
2023-04-20 00:34:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mylibrary/BookActivity.java
@@ -0,0 +1,195 @@
+package com.example.mylibrary;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class BookActivity extends AppCompatActivity {
+
+    public static final String BOOK_ID_KEY = "bookId";
+    private TextView textBookName, txtAuthorName, txtPages, txtDescription;
+    private Button btnAddToWantToRead, btnAddToAlreadyRead, btnAddToCurrentlyReading, btnAddToFavorite;
+    private ImageView bookImage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+
+        initViews();
+
+//        String longDescription = "A young woman named Aomame follows a taxi driver’s enigmatic suggestion and begins to notice puzzling discrepancies in the world around her. She has entered, she realizes, a parallel existence, which she calls 1Q84 —“Q is for ‘question mark.’ A world that bears a question.” Meanwhile, an aspiring writer named Tengo takes on a suspect ghostwriting project. He becomes so wrapped up with the work and its unusual author that, soon, his previously placid life begins to come unraveled.\n" +
+//                "\n" +
+//                "As Aomame’s and Tengo’s narratives converge over the course of this single year, we learn of the profound and tangled connections that bind them ever closer: a beautiful, dyslexic teenage girl with a unique vision; a mysterious religious cult that instigated a shoot-out with the metropolitan police; a reclusive, wealthy dowager who runs a shelter for abused women; a hideously ugly private investigator; a mild-mannered yet ruthlessly efficient bodyguard; and a peculiarly insistent television-fee collector.\n" +
+//                "\n" +
+//                "A love story, a mystery, a fantasy, a novel of self-discovery, a dystopia to rival George Orwell’s — 1Q84 is Haruki Murakami’s most ambitious undertaking yet: an instant best seller in his native Japan, and a tremendous feat of imagination from one of our most revered contemporary writers";
+//
+//        //TODO: Get the data from recycler view in here
+//        Book book = new Book(1, "1Q84", "Haruki Murakami","https://images-na.ssl-images-amazon.com/images/I/41FdmYnaNuL._SX322_BO1,204,203,200_.jpg",1350,
+//                "A work of maddening brilliance", longDescription);
+
+        Intent intent = getIntent();
+        if (null != intent) {
+            int bookId = intent.getIntExtra(BOOK_ID_KEY, -1);
+            if (bookId != -1) {
+                Book incomingBook = Utils.getInstance(this).getBookById(bookId);
+                if (null != incomingBook) {
+                    setData(incomingBook);
+
+                    handleAlreadyRead(incomingBook);
+                    handleWantToReadBooks(incomingBook);
+                    handleCurrentlyReadingBooks(incomingBook);
+                    handleFavoriteBooks(incomingBook);
+                }
+            }
+        }
+    }
+
+    private void handleFavoriteBooks(final Book book){
+        ArrayList<Book> favoriteBooks = Utils.getInstance(this).getFavouriteBooks();
+
+        boolean existInFavoriteBooks = false;
+
+        for (Book b : favoriteBooks) {
+            if (b.getId() == book.getId()) {
+                existInFavoriteBooks = true;
+            }
+        }
+        if (existInFavoriteBooks) {
+            btnAddToFavorite.setEnabled(false);
+        } else {
+            btnAddToFavorite.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (Utils.getInstance(BookActivity.this).addToFavorite(book)) {
+                        Toast.makeText(BookActivity.this, "Book Added", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(BookActivity.this, FavoriteActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(BookActivity.this, "Something Wrong happened, try again", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+    private void handleCurrentlyReadingBooks(final Book book){
+        ArrayList<Book> currentlyReadingBook = Utils.getInstance(this).getCurrentlyReadingBooks();
+
+        boolean existInCurrentlyReadingBooks = false;
+
+        for (Book b : currentlyReadingBook) {
+            if (b.getId() == book.getId()) {
+                existInCurrentlyReadingBooks = true;
+            }
+        }
+        if (existInCurrentlyReadingBooks) {
+            btnAddToCurrentlyReading.setEnabled(false);
+        } else {
+            btnAddToCurrentlyReading.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (Utils.getInstance(BookActivity.this).addToCurrentlyReading(book)) {
+                        Toast.makeText(BookActivity.this, "Book Added", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(BookActivity.this, CurrentlyReadingActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(BookActivity.this, "Something Wrong happened, try again", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void handleWantToReadBooks(final Book book){
+        ArrayList<Book> wantToReadBooks = Utils.getInstance(this).getWantToReadBooks();
+
+        boolean existInWantToReadBooks = false;
+
+        for (Book b : wantToReadBooks) {
+            if (b.getId() == book.getId()) {
+                existInWantToReadBooks = true;
+            }
+        }
+        if (existInWantToReadBooks) {
+            btnAddToWantToRead.setEnabled(false);
+        } else {
+            btnAddToWantToRead.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (Utils.getInstance(BookActivity.this).addToWantToRead(book)) {
+                        Toast.makeText(BookActivity.this, "Book Added", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(BookActivity.this, WantToReadActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(BookActivity.this, "Something Wrong happened, try again", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    /**
+     * Enable and Disable Button
+     * Add the book to Already Read Book ArrayList
+     * @param book
+     */
+    private void handleAlreadyRead(final Book book) {
+        ArrayList<Book> alreadyReadBooks = Utils.getInstance(this).getAlreadyReadBooks();
+
+        boolean existAlreadyReadBooks = false;
+
+        for (Book b : alreadyReadBooks) {
+            if (b.getId() == book.getId()) {
+                existAlreadyReadBooks = true;
+            }
+        }
+        if (existAlreadyReadBooks) {
+            btnAddToAlreadyRead.setEnabled(false);
+        } else {
+            btnAddToAlreadyRead.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (Utils.getInstance(BookActivity.this).addToAlreadyRead(book)) {
+                        Toast.makeText(BookActivity.this, "Book Added", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(BookActivity.this, AlreadyReadBookActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(BookActivity.this, "Something Wrong happened, try again", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+    private void setData(@NonNull Book book) {
+        textBookName.setText(book.getName());
+        txtAuthorName.setText(book.getAuthor());
+        txtPages.setText(String.valueOf(book.getPages()));
+        txtDescription.setText(book.getLongDesc());
+        Glide.with(this)
+                .asBitmap().load(book.getImageUrl())
+                .into(bookImage);
+    }
+    private void initViews() {
+        txtAuthorName = findViewById(R.id.txtAuthorName);
+        textBookName = findViewById(R.id.textBookName);
+        txtPages = findViewById(R.id.txtPages);
+        txtDescription = findViewById(R.id.txtDescription);
+
+        btnAddToWantToRead = findViewById(R.id.btnAddToWantToReadList);
+        btnAddToCurrentlyReading = findViewById(R.id.btnAddToCurrentlyReading);
+        btnAddToFavorite = findViewById(R.id.btnAddToFavourite);
+        btnAddToAlreadyRead = findViewById(R.id.btnAddToAlreadyReadList);
+
+        bookImage = findViewById(R.id.imageBook);
+    }
+}
